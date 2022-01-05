3ba020f26eba2f95a42cbb7ef020d32f4e4563cb
mbugowski1 <mbugowski1@wp.pl>
2022-01-05 14:51:47 +0100

    Added Card class

---
+++ cards.py
@@ -0,0 +1,26 @@
+class Card:
+    def __init__(self, x, y):
+        width = 100
+        height = 150
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y
+        self.x3 = x + width
+        self.y3 = y - height
+        self.x4 = x
+        self.y4 = y - height
+        self.z = 1
+    def get2(self):
+        return (self.x1, self.y1, self.x2, self.y2, self.x3, self.y3, self.x4, self.y4)
+    def get3(self):
+        return (self.x1, self.y1, self.z, self.x2, self.y2, self.z, self.x3, self.y3, self.z, self.x4, self.y4, self.z)
+    def move(self, x, y):
+        self.x1 += x
+        self.x2 += x
+        self.x3 += x
+        self.x4 += x
+        self.y1 += y
+        self.y2 += y
+        self.y3 += y
+        self.y4 += y
--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import pyglet as pg
-
+from cards import Card
+def x(percentage):
+    return percentage * window.width / 100
+def y(percentage):
+    return percentage * window.height / 100
 
 window = pg.window.Window()
+pg.gl.glClearColor(0, 1, 0, 1)
 label = pg.text.Label('UNO',
                             font_name = "Times New Roman",
                             font_size = 36,
                             x = window.width//2,
                             y = window.width//2,
                             anchor_x = 'center', anchor_y = 'center')
-image = pg.resource.image('logo.png')
-#image.scale = (min(image.height, window.height)/max(image.height, window.height), max(min(window.width, image.width)/max(window.width, image.width)))
-
-#image.width = window.width
-#image.height = window.height
-#image.texture.width = window.width
-#image.texture.height = window.height
-
+card1 = Card(300, 300)
 @window.event
 def on_draw():
     window.clear()
-    pg.graphics.draw_indexed(4, pg.gl.GL_TRIANGLES,
-        [0, 1, 2, 0, 2, 3],
-        ('v2i', (0, 0,
-                 0, window.height,
-                 window.width, window.height,
-                 window.width, 0))
+    pg.graphics.draw(4, pg.gl.GL_QUADS,
+    ('v3i', card1.get3())
     )
-    image.blit(0,window.height - image.height - 5)
     label.draw()
 pg.app.run()
