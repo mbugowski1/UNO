b68edf053425456ee4af15ebd466c60bddae3c9f
mbugowski1 <mbugowski1@wp.pl>
2022-01-11 17:06:37 +0100

    Rewrite of project

--- cards.py
+++ cards.py
@@ -1,43 +1,150 @@
-import pyglet as pg
-from addons import *
-class Card:
-    def drawCircle(self, x, y, edges):
-        vertices = circle(self.x+x, self.y+y, self.r, edges)
-        vertex = pg.graphics.vertex_list(edges, ('v3f', vertices))
-        return vertex
-    def drawEdge(self, x, y, width, height):
-        vertices = [self.x+x, self.y+y+height, 0.0, self.x+x+width, self.y+y+height, 0.0, self.x+x+width, self.y+y, 0.0, self.x+x, self.y+y, 0.0]
-        vertex = pg.graphics.vertex_list(4, ('v3f', vertices))
-        return vertex
-    def __init__(self, x, y, width, height, radius):
-        self.r = radius
-        self.width = width
+from pyglet.graphics import *
+import math
+class Cylinder:
+    def __init__(self, radius, height, frontColor, edgeColor):
+        self.radius = radius
         self.height = height
+
+        triangles = 100
+        circle = math.pi * 2
+        delta = circle/triangles
+        addon = 0
+        #vertexes
+        top = [0.0, 0.0, 0.0]
+        bottom = [0.0, 0.0, height]
+        edges = []
+        while (addon < circle):
+            #podstawa gorna
+            top.append(math.sin(addon) * self.radius)
+            top.append(math.cos(addon) * self.radius)
+            top.append(0.0)
+
+            #podstawa dolna
+            bottom.append(math.sin(addon) * self.radius)
+            bottom.append(math.cos(addon) * self.radius)
+            bottom.append(height)
+            
+            #bok
+            edges.append(math.sin(addon) * self.radius)
+            edges.append(math.cos(addon) * self.radius)
+            edges.append(0)
+            edges.append(math.sin(addon) * self.radius)
+            edges.append(math.cos(addon) * self.radius)
+            edges.append(height)
+
+            addon += delta
+        #podstawa gorna
+        top.append(math.sin(addon) * self.radius)
+        top.append(math.cos(addon) * self.radius)
+        top.append(0)
+
+        #podstawa dolna
+        bottom.append(math.sin(addon) * self.radius)
+        bottom.append(math.cos(addon) * self.radius)
+        bottom.append(height)
+            
+        #bok
+        edges.append(math.sin(addon) * self.radius)
+        edges.append(math.cos(addon) * self.radius)
+        edges.append(0)
+        edges.append(math.sin(addon) * self.radius)
+        edges.append(math.cos(addon) * self.radius)
+        edges.append(height)
+
+        frontColor *= len(top) // 3
+        edgeColor *= len(edges) // 3
+        self.top = vertex_list(len(top)//3, ('v3f', top), ('c3f', frontColor))
+        self.bottom = vertex_list(len(bottom)//3, ('v3f', bottom), ('c3f', frontColor))
+        self.edges = vertex_list(len(edges)//3, ('v3f', edges), ('c3f', edgeColor))
+    def draw(self):
+        self.top.draw(GL_TRIANGLE_FAN)
+        self.bottom.draw(GL_TRIANGLE_FAN)
+        self.edges.draw(GL_QUAD_STRIP)
+class Card:
+    def CreateEdges(self, front, edge):
+        front *= 4
+        edge *= 4
+        z = [0.0, self.depth]
+        for zi in z:
+            vertex = [0.0, self.radius, zi,
+                    0.0, self.height-self.radius, zi,
+                    2*self.radius, self.height-self.radius, zi,
+                    2*self.radius, self.radius, zi]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', front))
+            vertex = [self.radius, self.height, zi,
+                    self.width-self.radius, self.height, zi,
+                    self.width-self.radius, self.height-2*self.radius, zi,
+                    self.radius, self.height-2*self.radius, zi]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', front))
+            vertex = [self.width-2*self.radius, self.height-self.radius, zi,
+                    self.width, self.height-self.radius, zi,
+                    self.width, self.radius, zi,
+                    self.width-2*self.radius, self.radius, zi]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', front))
+            vertex = [self.radius, 0.0, zi,
+                    self.radius, 2*self.radius, zi,
+                    self.width-self.radius, 2*self.radius, zi,
+                    self.width-self.radius, 0.0, zi]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', front))
+            vertex = [self.radius, 0.0, 0.0,
+                    self.width-self.radius, 0.0, 0.0,
+                    self.width-self.radius, 0.0, self.depth,
+                    self.radius, 0.0, self.depth]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', edge))
+            vertex = [0.0, self.radius, 0.0,
+                    0.0, self.height-self.radius, 0.0,
+                    0.0, self.height-self.radius, self.depth,
+                    0.0, self.radius, self.depth]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', edge))
+            vertex = [self.radius, self.height, 0.0,
+                    self.width-self.radius, self.height, 0.0,
+                    self.width-self.radius, self.height, self.depth,
+                    self.radius, self.height, self.depth]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', edge))
+            vertex = [self.width, self.radius, 0.0,
+                    self.width, self.height-self.radius, 0.0,
+                    self.width, self.height-self.radius, self.depth,
+                    self.width, self.radius, self.depth]
+            self.batch.add(4, GL_QUADS, self.edges, ('v3f', vertex), ('c3f', edge))
+    def DrawCylinders(self):
+        glPushMatrix()
+        glTranslatef(self.radius, self.radius, 0.0)
+        self.cylinder.draw()
+        glTranslatef(0.0, self.height-2*self.radius, 0.0)
+        self.cylinder.draw()
+        glTranslatef(self.width-2*self.radius, 0.0, 0.0)
+        self.cylinder.draw()
+        glTranslatef(0.0, -self.height+2*self.radius, 0.0)
+        self.cylinder.draw()
+        glPopMatrix()
+    def createField(self, depth):
+        vertex = [self.radius, self.radius, depth,
+                self.radius, self.height-self.radius, depth,
+                self.width-self.radius, self.height-self.radius, depth,
+                self.width-self.radius, self.radius, depth]
+        color = [1.0, 0.0, 0.0] * 4
+        return vertex_list(4, ('v3f', vertex), ('c3f', color))
+    def __init__(self, x, y):
+        self.batch = Batch()
+        self.radius = 5.0
+        self.depth = -1.0
+        self.width = 100.0
+        self.height = 200.0
+        self.edges = Group()
         self.x = x
         self.y = y
-        #vertices
-        self.circle1 = self.drawCircle(self.r, height-self.r, 1000)
-        self.circle2 = self.drawCircle(width-self.r, height-self.r, 1000)
-        self.circle3 = self.drawCircle(width-self.r, self.r, 1000)
-        self.circle4 = self.drawCircle(self.r, self.r, 1000)
 
-        #edges
-        self.edge1 = self.drawEdge(self.r, height-2*self.r, width-2*self.r, 2*self.r)
-        self.edge2 = self.drawEdge(width+-2*self.r, self.r, 2*self.r, height-2*self.r)
-        self.edge3 = self.drawEdge(self.r, 0.0, width-2*self.r, 2*self.r)
-        self.edge4 = self.drawEdge(0, self.r, 2*self.r, height-2*self.r)
+        #declare elements
+        self.cylinder = Cylinder(self.radius, self.depth, [1.0, 1.0, 1.0], [0.0, 0.0, 0.0])
+        self.CreateEdges([1.0, 1.0, 1.0], [0.0, 0.0, 0.0])
+        self.front = self.createField(0.1)
+        self.back = self.createField(self.depth-0.1)
 
-        #middle
-        self.middle = self.drawEdge(self.r, self.r, width-2*self.r, height-2*self.r)
-    def render(self):
-        self.circle1.draw(pg.gl.GL_POLYGON)
-        self.circle2.draw(pg.gl.GL_POLYGON)
-        self.circle3.draw(pg.gl.GL_POLYGON)
-        self.circle4.draw(pg.gl.GL_POLYGON)
-
-        self.edge1.draw(pg.gl.GL_QUADS)
-        self.edge2.draw(pg.gl.GL_QUADS)
-        self.edge3.draw(pg.gl.GL_QUADS)
-        self.edge4.draw(pg.gl.GL_QUADS)
-
-        self.middle.draw(pg.gl.GL_QUADS)
+    def draw(self):
+        glPushMatrix()
+        glTranslatef(self.x, self.y, 0)
+        self.batch.draw()
+        self.DrawCylinders()
+        self.front.draw(GL_QUADS)
+        self.back.draw(GL_QUADS)
+        glPopMatrix()
--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import pyglet as pg
-from cards import *
-def x(percentage):
-    return percentage * window.width / 100
-def y(percentage):
-    return percentage * window.height / 100
+from cards import Card2
 
-window = pg.window.Window()
-pg.gl.glClearColor(0, 1, 0, 1)
-label = pg.text.Label('UNO',
-                            font_name = "Times New Roman",
-                            font_size = 36,
-                            x = window.width//2,
-                            y = window.width//2,
-                            anchor_x = 'center', anchor_y = 'center')
-cardImage = pg.image.load('reverse.png')
-card2 = pg.sprite.Sprite(cardImage, 100, 150)
-card2.scale = 0.3
-card2.rotation = 0
-card1 = Card(50, 50, 200, 300, 20)
-batch = pg.graphics.Batch()
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
-    card1.render()
-pg.app.run()
+distance = 500
+rotOX = -45.0
+rotOY = -45.0
+
+def draw():
+    quadric = pg.gl.gluNewQuadric()
+    pg.gl.gluQuadricDrawStyle(quadric, pg.gl.GLU_LINE)
+    pg.graphics.gluCylinder(quadric, 20.0, 20.0, 30.0, 100, 1)
+
+class Window(pg.window.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_minimum_size(300, 200)
+        pg.gl.glMatrixMode(pg.gl.GL_PROJECTION)
+        pg.gl.glLoadIdentity()
+        pg.gl.gluPerspective(45.0, self.width/self.height, 1.0, 1000.0)
+        pg.gl.glMatrixMode(pg.gl.GL_MODELVIEW)
+        pg.gl.glLoadIdentity()
+        self.card = Card2(0, 0)
+
+    def on_resize(self, width, height):
+        pg.gl.glViewport(0, 0, width, height)
+    def on_draw(self):
+        self.clear()
+        pg.graphics.glPushMatrix()
+        pg.graphics.glTranslatef(0, 0, -distance)
+        pg.graphics.glRotatef(rotOX, 1, 0, 0)
+        pg.graphics.glRotatef(rotOY, 0, 1, 0)
+        #drawing models
+        self.card.draw()
+        pg.graphics.glPopMatrix()
+        
+
+if __name__ == '__main__':
+    config = pg.gl.Config(sample_buffers=1, samples=4)
+    window = Window(width=1280, height=720, caption='UNO', config=config, resizable=True)
+    pg.gl.glClearColor(0.5, 0.7, 1, 1)
+    pg.gl.glEnable(pg.gl.GL_DEPTH_TEST)
+    pg.app.run()
